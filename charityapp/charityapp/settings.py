2afcde1d1d98521dea3889d1e64dbbcd9ec7974b
LyubomiraMihova <lyubomiramihova@gmail.com>
2023-07-26 21:59:58 +0300

    Adding .env to .gitignore

--- charityapp/charityapp/settings.py
+++ charityapp/charityapp/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 
+from decouple import config
 from django.urls import reverse_lazy
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -84,11 +85,11 @@ WSGI_APPLICATION = 'charityapp.wsgi.application'
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": "charity_db",
-        "USER": "LyubomiraMihova",
-        "PASSWORD": "Liubomira1!",
-        "HOST": "127.0.0.1",
-        "PORT": "5432",
+        "NAME": config("DATABASE_NAME"),
+        "USER": config("DATABASE_USER"),
+        "PASSWORD": config("DATABASE_PASSWORD"),
+        "HOST": config("DATABASE_HOST"),
+        "PORT": config("DATABASE_PORT"),
     }
 }
 
