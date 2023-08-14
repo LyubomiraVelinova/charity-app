17c6b064cd6043d2c65a0ad23607473074705dcf
LyubomiraMihova <lyubomiramihova@gmail.com>
2023-08-14 19:24:34 +0300

    Modifications

--- charityapp/charityapp/user_accounts/forms.py
+++ charityapp/charityapp/user_accounts/forms.py
@@ -2,8 +2,6 @@ from django import forms
 from django.contrib.auth import forms as auth_forms, get_user_model
 from django.utils.translation import gettext_lazy as _
 
-from charityapp.user_profiles.models import SponsorProfile, VolunteerProfile, MemberProfile
-
 UserModel = get_user_model()
 
 
